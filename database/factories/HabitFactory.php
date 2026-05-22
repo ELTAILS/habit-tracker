913a45aeadad6576409a1bd8df3180c856f5a48f
Wagner da silva Junior <wagnerjr306@gmail.com>
2026-05-22 15:10:53 -0300

    Crie factory e seeder das tabelas do projeto

---
+++ database/factories/HabitFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Habit;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Habit>
+ */
+class HabitFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => '1',
+            'name' => $this->faker->name(), 
+        ];
+    }
+}
